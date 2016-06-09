4a88743c182002d12fc8713831ae81710566a599
Brian Miller <bzmiller82@gmail.com>
2016-06-08 23:38:14 -0400

    A few sections created, cleaned up jekyll

---
+++ gulpfile.js
@@ -0,0 +1,54 @@
+var gulp        = require('gulp'),
+    sass        = require ('gulp-sass'),
+    prefix      = require('gulp-autoprefixer'),
+    plumber     = require ('gulp-plumber'),
+    cp          = require('child_process'),
+    browserSync = require('browser-sync');
+
+var jekyll   = process.platform === 'win32' ? 'jekyll.bat' : 'jekyll';
+var messages = {
+    jekyllBuild: '<span style="color: grey">Running:</span> $ jekyll build'
+};
+
+// Build the Jekyll Site
+gulp.task('jekyll-build', function (done) {
+    browserSync.notify(messages.jekyllBuild);
+    return cp.spawn( jekyll , ['build'], {stdio: 'inherit'})
+        .on('close', done);
+});
+
+// Rebuild Jekyll & do page reload
+gulp.task('jekyll-rebuild', ['jekyll-build'], function () {
+    browserSync.reload();
+});
+
+// Wait for jekyll-build, then launch the Server
+gulp.task('browser-sync', ['styles', 'jekyll-build'], function() {
+    browserSync({
+        server: {
+            baseDir: '_site'
+        }
+    });
+});
+
+// Compile files from _scss into both _site/css (for live injecting) and site (for future jekyll builds)
+gulp.task ('styles', function() {
+  return gulp.src('_scss/main.scss')
+    .pipe(plumber())
+    .pipe(sass())
+    .pipe(prefix(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true }))
+    .pipe(gulp.dest('_site/css'))
+    .pipe(browserSync.stream())
+    .pipe(gulp.dest('css'));
+});
+
+/**
+ * Watch scss files for changes & recompile
+ * Watch html/md files, run jekyll & reload BrowserSync
+ */
+gulp.task('watch', function () {
+    gulp.watch('_scss/*.scss', ['styles']);
+    gulp.watch(['*.html', '_includes/*.html', '_layouts/*.html', '_posts/*'], ['jekyll-rebuild']);
+});
+
+gulp.task ('default', ['browser-sync', 'watch']);
